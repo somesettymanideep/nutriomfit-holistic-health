103da8d5802b1a2aa871f52dac2609cd1e492393
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2025-12-07 18:17:00 +0530

    s

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
+function spaFallback() {
+  return {
+    name: "spa-fallback",
+    closeBundle() {
+      const fs = require("fs");
+      const indexHtml = fs.readFileSync("./dist/index.html", "utf-8");
+      fs.writeFileSync("./dist/404.html", indexHtml);
+    },
+  };
+}
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/nutriomfit-holistic-health/",
@@ -10,7 +21,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger(),spaFallback()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
