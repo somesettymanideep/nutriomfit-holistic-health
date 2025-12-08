638495b0228acaed885c9f27279fd1161046d416
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2025-12-08 23:51:23 +0530

    dv

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/nutriomfit-holistic-health",
+  base: "/nutriomfit-holistic-health/",
   server: {
     host: "::",
     port: 8080,
