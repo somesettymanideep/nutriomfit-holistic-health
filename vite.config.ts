bcc588cb9e4e319a57d60c980f3a0ee4aff8522d
somesetty manideep <manideep723@gmail.com>
2025-12-09 10:43:00 +0530

    dcdc

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // repo name
-
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
-
+  base: "/",
+  // base: mode === "production" ? "/nutriomfit-harmony-hub/" : "/",
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
