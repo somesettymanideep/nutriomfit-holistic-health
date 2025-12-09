cfa73e325e0d01f3bea7d1d2efa80f54aa470536
somesetty manideep <manideep723@gmail.com>
2025-12-09 10:34:01 +0530

    dvv

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/nutriomfit-holistic-health/", // repo name
+  base: "/", // repo name
 
   plugins: [
     react(),
